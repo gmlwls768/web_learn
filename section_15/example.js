56daaac07abdec52365d0954228a28c41f247e03
KIMHEEJIN <gmlwls768@naver.com>
2023-01-29 21:48:11 +0900

    web simple practice

---
+++ section_15/example.js
@@ -0,0 +1,27 @@
+// Define lottoNumbers below:
+let lottoNumbers = [1,12,3,4,5,5]
+
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = "Luna"
+leaderboard[3] = "Draco"
+
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift()
+planets.push("Saturn")
+planets.unshift("Mercury")
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+
+airplaneSeats[3][1] = "Hugo"
