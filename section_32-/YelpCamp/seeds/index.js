061af91fd3cfb517396fa7de31aa8c11564bb715
KIMHEEJIN <gmlwls768@naver.com>
2023-03-22 21:38:22 +0900

    web simple practice

--- section_32-/YelpCamp/seeds/index.js
+++ section_32-/YelpCamp/seeds/index.js
@@ -17,14 +17,19 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 const seedDB = async()=>{
     await Campground.deleteMany({})
     for(let i = 0; i<50 ; i++){
-        const random1000 =Math.floor(Math.random() * 1000)
+        const random1000 =Math.floor(Math.random() * 1000);
+        const price =Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save()
-    }
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, et. Ea earum ratione ad beatae aperiam consequatur totam laboriosam, voluptate consectetur voluptates dolorem quae! Similique ipsum sint mollitia maxime assumenda.',
+            price
+        });
+        await camp.save();
+    }   
 }
-seedDB.then(()=>{
-    mongoose.connection.close()
+
+seedDB().then(() => {
+    mongoose.connection.close();
 })
